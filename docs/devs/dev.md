b1c0646b9d0903d542a7f15c887b8d82c0354efd
hotchilipowder <h12345jack@gmail.com>
2025-02-03 22:18:01 +0800

    feat: add rewrite cookiecutter value

--- docs/devs/dev.md
+++ docs/devs/dev.md
@@ -10,3 +10,24 @@
 
 
 ## TODO
+- [ ] Bug for project name
+
+
+
+### Findings
+
+https://cookiecutter.readthedocs.io/en/stable/advanced/hooks.html
+
+hooks中, `pre_prompt` 
+使用
+
+```
+current_folder = os.path.basename(os.getcwd())
+
+```
+得到的是模板仓库的目录名，而不是你启动 cookiecutter 时所在的目录名或者最终生成项目的文件夹名。
+
+如果确实需要，需要在调用时传入，太麻烦了...
+想办法
+
+修改为默认是default, 如果default那么就会修改为项目名，如果想要改，那么就改.
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,1 +1,30 @@
+import os
+import re
+import json
 
+# 获取当前目录名称
+current_folder = os.path.basename(os.getcwd())
+
+# 如果需要对文件夹名做一些处理，比如将非字母数字字符替换掉
+def slugify(value):
+    value = value.lower().strip()
+    value = re.sub(r'\s+', '-', value)            # 把空格替换为 -
+    value = re.sub(r'[^\w\-]', '', value)          # 移除非字母数字和非连字符
+    value = re.sub(r'[-]+', '-', value)            # 合并重复的-
+    return value
+
+# 如果用户在交互时输入了其他值，可以在这里根据需要选择是否覆盖
+# 这里假设你希望总是以当前目录为准
+project_name = slugify(current_folder)
+import os
+import re
+import sys
+
+print("当前全局变量：", globals().keys(), file=sys.stderr)
+
+# 以下的代码假设 cookiecutter 已被注入
+try:
+    cc = cookiecutter
+except NameError:
+    print("错误：全局变量 'cookiecutter' 不存在，请确保使用 cookiecutter CLI 生成项目！", file=sys.stderr)
+    sys.exit(1)
